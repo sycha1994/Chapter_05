8930130ed3b11ec22ba5e2d729c03fa751ab9e87
sycha1994 <sycha1994@gmail.com>
2021-07-19 23:46:32 +0900

    switch식

--- MainApp.cs
+++ MainApp.cs
@@ -1,4 +1,4 @@
-﻿using static System.Console;
+﻿using System;
 
 namespace Chapter_05
 {
@@ -6,31 +6,24 @@ namespace Chapter_05
     {
         static void Main(string[] args)
         {
-            object obj = null;
+            Console.WriteLine("점수를 입력하세요");
+            int score = Convert.ToInt32(Console.ReadLine());
 
-            string s = ReadLine();
-            if (int.TryParse(s, out int out_i)) 
-                obj = out_i;
-            else if (float.TryParse(s, out float out_f))
-                obj = out_f;
-            else
-                obj = s;
+            Console.WriteLine("재수강인가요? (y/n)");
+            string line = Console.ReadLine();
+            bool repeated = line == "y" ? true : false;
 
-            switch (obj)
+            string grade = (int)(Math.Truncate(score / 10.0) * 10) switch
             {
-                case int i when i >= 0:
-                    WriteLine($"{i}는 int 형식이며 양수입니다");
-                    break;
-                case int i:
-                    WriteLine($"{i}는 int 형식이며 음수입니다.");
-                    break;
-                case float f:
-                    WriteLine($"{f}는 float 형식입니다.");
-                    break;
-                default:
-                    WriteLine($"{obj}(은)는 모르는 형식입니다.");
-                    break;
-            }
+                90 when repeated == true => "B+",
+                90 => "A",
+                80 => "B",
+                70 => "C",
+                60 => "D",
+                _ => "F"
+            };
+
+            Console.WriteLine($"학점 : {grade}");
         }
     }
 }
