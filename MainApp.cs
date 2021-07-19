329d38f41227da65338ed385b74e90217caa658c
sycha1994 <sycha1994@gmail.com>
2021-07-19 21:54:40 +0900

    switch문

--- MainApp.cs
+++ MainApp.cs
@@ -6,7 +6,28 @@ namespace Chapter_05
     {
         static void Main(string[] args)
         {
-            WriteLine("Hello World!");
+            object obj = null;
+
+            string s = ReadLine();
+            if (int.TryParse(s, out int out_i))
+                obj = out_i;
+            else if (float.TryParse(s, out float out_f))
+                obj = out_f;
+            else
+                obj = s;
+
+            switch (obj)
+            {
+                case int i:
+                    WriteLine($"{i}는 int 형식입니다.");
+                    break;
+                case float f:
+                    WriteLine($"{f}는 float 형식입니다.");
+                    break;
+                default:
+                    WriteLine($"{obj}(은)는 모르는 형식입니다.");
+                    break;
+            }
         }
     }
 }
