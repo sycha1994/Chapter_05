02dca1d58bb476e70a368884917427f56e526149
sycha1994 <sycha1994@gmail.com>
2021-07-19 21:46:32 +0900

    초기 커밋.

---
+++ MainApp.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+namespace Chapter_05
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+        }
+    }
+}
