e1f5e78860ba42855076eeb1f6ac25ce80cdfaa1
sycha1994 <sycha1994@gmail.com>
2021-07-19 23:50:21 +0900

    foreach문

--- MainApp.cs
+++ MainApp.cs
@@ -6,24 +6,12 @@ namespace Chapter_05
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("점수를 입력하세요");
-            int score = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[] { 0, 1, 2, 3, 4 };
 
-            Console.WriteLine("재수강인가요? (y/n)");
-            string line = Console.ReadLine();
-            bool repeated = line == "y" ? true : false;
-
-            string grade = (int)(Math.Truncate(score / 10.0) * 10) switch
+            foreach (int a in arr)
             {
-                90 when repeated == true => "B+",
-                90 => "A",
-                80 => "B",
-                70 => "C",
-                60 => "D",
-                _ => "F"
-            };
-
-            Console.WriteLine($"학점 : {grade}");
+                Console.WriteLine(a);
+            }
         }
     }
 }
